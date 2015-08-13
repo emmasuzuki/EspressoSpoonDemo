bd90132f22395a412560dedd295686c170674b09
Emma Suzuki <emma.suzuki@capitalone.com>
2015-08-13 10:19:11 -0700

    Junit4 syntax

--- app/src/androidTest/java/com/emmasuzuki/espressospoondemo/LoginActivityTest.java
+++ app/src/androidTest/java/com/emmasuzuki/espressospoondemo/LoginActivityTest.java
@@ -19,7 +19,7 @@
  */
 package com.emmasuzuki.espressospoondemo;
 
-import android.test.ActivityInstrumentationTestCase2;
+import android.support.test.rule.ActivityTestRule;
 import android.view.View;
 import android.widget.EditText;
 
@@ -28,6 +28,10 @@ import com.squareup.spoon.Spoon;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -37,20 +41,21 @@ import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
-public class LoginActivityTest extends ActivityInstrumentationTestCase2<LoginActivity> {
+public class LoginActivityTest {
 
-    public LoginActivityTest() {
-        super(LoginActivity.class);
+    @Rule
+    public ActivityTestRule<LoginActivity> mActivityTestRule = new ActivityTestRule<>(LoginActivity.class);
+
+    private LoginActivity mActivity;
+
+    @Before
+    public void setUp() {
+        mActivity = mActivityTestRule.getActivity();
     }
 
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-        getActivity();
-    }
-
+    @Test
     public void testSetEmailInvalidError() {
-        Spoon.screenshot(getActivity(), "initial_state");
+        Spoon.screenshot(mActivity, "initial_state");
 
         // When view is hidden by keyboard perform("action") fails to do action.
 
@@ -61,33 +66,35 @@ public class LoginActivityTest extends ActivityInstrumentationTestCase2<LoginAct
 
         onView(withId(R.id.submit)).perform(click());
 
-        onView(withId(R.id.email)).check(matches(hasErrorText(getActivity().getString(R.string.msg_email_error))));
+        Spoon.screenshot(mActivity, "after_login");
 
-        Spoon.screenshot(getActivity(), "after_login");
+        onView(withId(R.id.email)).check(matches(hasErrorText(mActivity.getString(R.string.msg_email_error))));
     }
 
+    @Test
     public void testSetPasswordInvalidError() {
-        Spoon.screenshot(getActivity(), "initial_state");
+        Spoon.screenshot(mActivity, "initial_state");
 
         onView(withId(R.id.email)).perform(typeText("test@test.com\n"));
         onView(withId(R.id.password)).perform(typeText("\n"));
 
         onView(withId(R.id.submit)).perform(click());
 
-        Spoon.screenshot(getActivity(), "after_login");
+        Spoon.screenshot(mActivity, "after_login");
 
-        onView(withId(R.id.password)).check(matches(hasErrorText(getActivity().getString(R.string.msg_password_error))));
+        onView(withId(R.id.password)).check(matches(hasErrorText(mActivity.getString(R.string.msg_password_error))));
     }
 
+    @Test
     public void testSetMismatchError() {
-        Spoon.screenshot(getActivity(), "initial_state");
+        Spoon.screenshot(mActivity, "initial_state");
 
         onView(withId(R.id.email)).perform(typeText("espresso@spoon.com\n"));
         onView(withId(R.id.password)).perform(typeText("bananacake\n\n"));
 
         onView(withId(R.id.submit)).perform(click());
 
-        Spoon.screenshot(getActivity(), "after_login");
+        Spoon.screenshot(mActivity, "after_login");
 
         onView(withText(R.string.msg_mismatch)).check(matches(isDisplayed()));
     }

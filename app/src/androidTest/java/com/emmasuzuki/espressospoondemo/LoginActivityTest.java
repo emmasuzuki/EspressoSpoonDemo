9a323d59e87aabe07e0bc39a5e35ca2549e61809
Vinodh Raj <vinodh.raj@gmail.com>
2022-02-24 12:44:12 +0530

    test added

--- app/src/androidTest/java/com/emmasuzuki/espressospoondemo/LoginActivityTest.java
+++ app/src/androidTest/java/com/emmasuzuki/espressospoondemo/LoginActivityTest.java
@@ -1,11 +1,35 @@
 package com.emmasuzuki.espressospoondemo;
 
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
 public class LoginActivityTest {
+
+    @Rule
+    public ActivityTestRule<LoginActivity> activityActivityTestRule = new ActivityTestRule<>(LoginActivity.class);
+
+    private LoginActivity activity;
+
     @Test
-    public void sample() {
+    public void testInvalidEmail() throws InterruptedException {
+        activity = activityActivityTestRule.getActivity();
+
+        onView(withId(R.id.email)).perform(typeText("test"));
+        onView(withId(R.id.password)).perform(typeText("phonepe"));
+
+        onView(withId(R.id.submit)).perform(click());
+
+        Thread.sleep(10000);
 
     }
+
 
 }
